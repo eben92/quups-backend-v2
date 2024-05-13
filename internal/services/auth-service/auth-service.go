ca49d450e1550840bbb7affbcbc97f545d867866
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-05-13 01:20:46 +0000

    fixes

--- internal/services/auth-service/auth-service.go
+++ internal/services/auth-service/auth-service.go
@@ -2,11 +2,14 @@ package services
 
 import (
 	"context"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	repository "quups-backend/internal/database/repository"
+	"quups-backend/internal/util"
 )
 
 type Service struct {
@@ -15,66 +18,75 @@ type Service struct {
 
 func (s *Service) Signin(w http.ResponseWriter, r *http.Request) {
 
-	res, err := s.Repo.GetUsers(r.Context())
+	response := util.Response{}
+	var res []byte
+
+	u, err := s.Repo.GetUsers(r.Context())
 
 	if err != nil {
 		fmt.Print(err.Error())
 
-		_, _ = w.Write([]byte(err.Error()))
+		res, _ = response.Builder(w, r, &util.ApiResponseParams{
+			Results:    err.Error(),
+			StatusCode: http.StatusBadRequest,
+			Message:    util.String("Error getting users. Please try again"),
+		})
+
+		_, _ = w.Write(res)
 		return
 	}
 
 	result := []userDTO{}
-	for i := 0; i < len(res); i++ {
-		u := mapToUserDTO(res[i])
+
+	for i := 0; i < len(u); i++ {
+		u := mapToUserDTO(u[i])
 		result = append(result, *u)
 	}
 
-	var response struct {
-		Results []userDTO `json:"results"`
-	}
-	response.Results = result
+	// w.WriteHeader(http.StatusCreated)
+	res, _ = response.Builder(w, r, &util.ApiResponseParams{
+		Results:    result,
+		StatusCode: http.StatusOK,
+		Message:    util.String("users retrieved successfully"),
+	})
 
-	data, err := json.Marshal(response)
-
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	_, _ = w.Write(data)
+	_, _ = w.Write(res)
 }
 
 type userBody struct {
-	Email *string
-}
-
-type ApiError struct {
-	StatusCode int    `json:"status_code"`
-	Message    string `json:"message"`
-	Result     any    `json:"result"`
+	Email    *string
+	Name     *string
+	Msisdn   string
+	Password string
 }
 
 func (s *Service) Signup(w http.ResponseWriter, r *http.Request) {
 	var body *userBody
+	response := util.Response{}
 
 	err := json.NewDecoder(r.Body).Decode(&body)
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 
-		_, _ = w.Write([]byte(err.Error()))
+		res, _ := response.Builder(w, r, &util.ApiResponseParams{
+			Results:    nil,
+			StatusCode: http.StatusBadRequest,
+			Message:    util.String(err.Error()),
+		})
+
+		_, _ = w.Write(res)
 		return
 	}
 
 	// check if email is nil
 	if body.Email == nil {
 		w.WriteHeader(http.StatusBadRequest)
-		res, _ := json.Marshal(&ApiError{
+
+		res, _ := response.Builder(w, r, &util.ApiResponseParams{
+			Results:    nil,
 			StatusCode: http.StatusBadRequest,
-			Message:    "Email field is required",
-			Result:     nil,
+			Message:    util.String("Email field is required"),
 		})
 
 		_, _ = w.Write(res)
@@ -93,20 +105,41 @@ func (s *Service) Signup(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		_, _ = w.Write([]byte(err.Error()))
+
+		res, _ := response.Builder(w, r, &util.ApiResponseParams{
+			Results:    nil,
+			StatusCode: http.StatusBadRequest,
+			Message:    util.String(err.Error()),
+		})
+
+		_, _ = w.Write(res)
 		return
 	}
 
 	new_u := mapToUserDTO(u)
-	res, _ := json.Marshal(new_u)
+
+	res, _ := response.Builder(w, r, &util.ApiResponseParams{
+		Results:    new_u,
+		StatusCode: http.StatusBadRequest,
+		Message:    util.String("user created successfully"),
+	})
 
 	_, _ = w.Write(res)
 }
 
 func (s *Service) createUser(ctx context.Context, body *userBody) (repository.User, error) {
 
+	n := *body.Name
+
+
 	u, err := s.Repo.CreateUser(ctx, repository.CreateUserParams{
 		Email: *body.Email,
+		Name: sql.NullString{
+			String: n,
+			Valid:  strconv.ParseBool(n); err != nil {
+				return false
+			},
+		},
 	})
 
 	if err != nil {
