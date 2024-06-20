74a96e15150894edf20af36becbcd896814fe06e
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-06-20 04:29:11 +0000

    update payment struct

--- internal/services/user-service/service/payment-service.go
+++ internal/services/user-service/service/payment-service.go
@@ -20,21 +20,21 @@ type apiResponse struct {
 }
 
 type Bank struct {
-	Active           bool      `json:"active"`
-	Code             string    `json:"code"`
-	Country          string    `json:"country"`
-	CreatedAt        time.Time `json:"createdAt"`
-	Currency         string    `json:"currency"`
-	Gateway          *string   `json:"gateway"`
-	ID               int       `json:"id"`
-	IsDeleted        bool      `json:"is_deleted"`
-	Longcode         string    `json:"longcode"`
-	Name             string    `json:"name"`
-	PayWithBank      bool      `json:"pay_with_bank"`
-	Slug             string    `json:"slug"`
-	SupportsTransfer bool      `json:"supports_transfer"`
-	Type             string    `json:"type"`
-	UpdatedAt        time.Time `json:"updatedAt"`
+	Active           bool        `json:"active"`
+	Code             string      `json:"code"`
+	Country          string      `json:"country"`
+	CreatedAt        time.Time   `json:"createdAt"`
+	Currency         string      `json:"currency"`
+	Gateway          interface{} `json:"gateway"`
+	ID               int         `json:"id"`
+	IsDeleted        bool        `json:"is_deleted"`
+	Longcode         string      `json:"longcode"`
+	Name             string      `json:"name"`
+	PayWithBank      bool        `json:"pay_with_bank"`
+	Slug             string      `json:"slug"`
+	SupportsTransfer bool        `json:"supports_transfer"`
+	Type             string      `json:"type"`
+	UpdatedAt        time.Time   `json:"updatedAt"`
 }
 
 func (s *service) GetBankList() (*[]Bank, error) {
