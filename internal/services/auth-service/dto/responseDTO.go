7092c0c3245908b253d71432606880e7aa120f5f
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-07-07 03:44:31 +0000

    chore: Update ResponseUserDTO struct fields

--- internal/services/auth-service/dto/responseDTO.go
+++ internal/services/auth-service/dto/responseDTO.go
@@ -1,11 +1,11 @@
 package authdto
 
 type ResponseUserDTO struct {
-	ID          string  `json:"id"`
-	Email       string  `json:"email"`
-	Name        *string `json:"name"`
-	Msisdn      *string `json:"msisdn"`
-	ImageUrl    *string `json:"image_url"`
-	Gender      *string `json:"gender"`
-	AccessToken *string `json:"access_token"`
+	ID          string `json:"id"`
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	Msisdn      string `json:"msisdn"`
+	ImageUrl    string `json:"image_url"`
+	Gender      string `json:"gender"`
+	AccessToken string `json:"access_token"`
 }
