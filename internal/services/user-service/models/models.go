c312720c3c7ab8442e2a573fe94013df499020af
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-08-07 08:25:16 +0000

    refactor: Add Address struct to user-service models

--- internal/services/user-service/models/models.go
+++ internal/services/user-service/models/models.go
@@ -15,3 +15,20 @@ const (
 	ACTIVE_STATUS   Status = "ACTIVE"
 	INACTIVE_STATUS Status = "INACTIVE"
 )
+
+type Address struct {
+	CompanyID        string  `json:"company_id"`
+	UserID           string  `json:"user_id"`
+	Msisdn           string  `json:"msisdn"`
+	IsDefault        bool    `json:"is_default"`
+	Latitude         float64 `json:"latitude"`
+	Longitude        float64 `json:"longitude"`
+	Description      string  `json:"description"`
+	FormattedAddress string  `json:"formatted_address"`
+	CountryCode      string  `json:"country_code"`
+	Region           string  `json:"region"`
+	Street           string  `json:"street"`
+	City             string  `json:"city"`
+	Country          string  `json:"country"`
+	PostalCode       string  `json:"postal_code"`
+}
