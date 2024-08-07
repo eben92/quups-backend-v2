103e415ba64a3e56158537fbc3422b4c0618891d
eben96 <pappy.rhutmannjnrolege@gmail.com>
2024-08-07 06:29:53 +0000

    refactor: Update CreateCompany function in MockCompanySvc to use userservice package

--- tests/mock/company.go
+++ tests/mock/company.go
@@ -1,8 +1,10 @@
 package mock_test
 
 import (
+	"context"
 	"quups-backend/internal/database"
 	userdto "quups-backend/internal/services/user-service/dto"
+	userservice "quups-backend/internal/services/user-service/service"
 )
 
 type MockCompanyMgt interface {
@@ -10,14 +12,34 @@ type MockCompanyMgt interface {
 }
 
 type MockCompanySvc struct {
-	db database.Service
+	db  database.Service
+	ctx context.Context
 }
 
-func NewMockCompanySvc(db database.Service) MockCompanyMgt {
-	return &MockCompanySvc{db: db}
+func NewMockCompanySvc(ctx context.Context, db database.Service) MockCompanyMgt {
+	return &MockCompanySvc{db: db, ctx: ctx}
+}
+
+func GetSampleCompany() userdto.CreateCompanyParams {
+	return userdto.CreateCompanyParams{
+		Name:           "Test Company",
+		Email:          "test@company.com",
+		Msisdn:         "0200000000",
+		About:          "We are a test company",
+		ImageUrl:       "https://test.com/image.jpg",
+		BannerUrl:      "https://test.com/banner.jpg",
+		Tin:            "123456",
+		BrandType:      "FOOD",
+		CurrencyCode:   "GHS",
+		InvitationCode: "INV123",
+	}
 }
 
 func (m *MockCompanySvc) CreateCompany(body userdto.CreateCompanyParams) (userdto.CompanyInternalDTO, error) {
 
-	return userdto.CompanyInternalDTO{}, nil
+	usersvc := userservice.NewCompanyService(m.ctx, m.db)
+
+	c, err := usersvc.CreateCompany(body)
+
+	return c, err
 }
